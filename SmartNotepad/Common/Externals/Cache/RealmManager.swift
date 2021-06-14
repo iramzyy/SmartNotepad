684c20454463d282dff6387a81dc9143bb216a7f
A. Ramzy <ramzy.2871@gmail.com>
2021-06-14 19:52:49 +0200

    Add RealmManager, Add Note model

---
+++ SmartNotepad/Common/Externals/Cache/RealmManager.swift
@@ -0,0 +1,62 @@
+//
+//  RealmManager.swift
+//  SmartNotepad
+//
+//  Created by Ramzy on 14/06/2021.
+//
+
+import Foundation
+import RealmSwift
+
+let realmObject = try! Realm()
+
+class RealmManager: NSObject {
+    
+    static let sharedInstance = RealmManager()
+    
+    func retrieveAllDataForObject(_ T : Object.Type) -> [Object] {
+        var objects = [Object]()
+        for result in realmObject.objects(T) {
+            objects.append(result)
+        }
+        return objects
+    }
+    
+    func deleteAllDataForObject(_ T : Object.Type) {
+        self.delete(self.retrieveAllDataForObject(T))
+    }
+    
+    func replaceAllDataForObject(_ T : Object.Type, with objects : [Object]) {
+        deleteAllDataForObject(T)
+        add(objects)
+    }
+    
+    func add(_ objects : [Object], completion : @escaping() -> ()) {
+        try! realmObject.write{
+            realmObject.add(objects)
+            completion()
+        }
+    }
+    
+    func add(_ objects : [Object]) {
+        try! realmObject.write{
+            realmObject.add(objects)
+        }
+    }
+    
+    func add(_ object: Object) {
+        try! realmObject.write{
+            realmObject.add(object)
+        }
+    }
+    
+    func update(_ block: @escaping ()->Void) {
+        try! realmObject.write(block)
+    }
+    
+    func delete(_ objects : [Object]) {
+        try! realmObject.write{
+            realmObject.delete(objects)
+        }
+    }
+}
---
+++ SmartNotepad/Scenes/NotesList/Note.swift
@@ -0,0 +1,23 @@
+//
+//  Note.swift
+//  SmartNotepad
+//
+//  Created by Ramzy on 14/06/2021.
+//
+
+import Foundation
+import RealmSwift
+
+class Note: Object {
+    dynamic var noteID = UUID().uuidString
+    @objc dynamic var noteTitle: String = "Note title"
+    @objc dynamic var noteBody: String = "Note body"
+    @objc dynamic var noteImageData: Data?  = nil
+    var noteLatitude = RealmOptional<Double>()
+    var noteLongitude = RealmOptional<Double>()
+    
+
+    override static func primaryKey() -> String? {
+      return "noteID"
+    }
+}
